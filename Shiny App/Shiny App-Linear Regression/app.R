5c24cea722f3bd80eaedd7528187f647d900cd0f
Lei Fang <lei.fang@outlook.de>
2017-01-18 19:49:40 +0100

    prototype of simple linear regression
    https://leifang.shinyapps.io/Shiny_App-Linear_Regression/

--- Shiny App/Shiny App-Linear Regression/app.R
+++ Shiny App/Shiny App-Linear Regression/app.R
@@ -26,29 +26,47 @@ ui = fluidPage(
       ),
       actionButton(inputId = "go",
                    label = "Update"
-      )
+      ),
+      
+      tags$hr(),
+      
+      helpText("This App employs ordinary least squares (OLS) to fit a linear 
+               regression to the data set you upload. The data must be stored in 
+               Excel file with header, and varible y is stored in the first column 
+               while x is the second one.")
+      
     ),
+        
     mainPanel(
       tableOutput('contents'),
-      tableOutput('parameters_estimation'),
-      plotOutput('estimation')
+      plotOutput('estimation'),
+      verbatimTextOutput('parameters_estimation')
     )
   )
 )
 
 server <- function(input, output) {
+  
   output$contents <- renderTable({
     inFile <- input$file1
-    
     if (is.null(inFile))
       return(NULL)
     file.rename(inFile$datapath,
                 paste(inFile$datapath, ".xlsx", sep = ""))
     read_excel(paste(inFile$datapath, ".xlsx", sep = ""), sheet = 1)
   })
-  data = eventReactive(eventExpr = input$go,
-               valueExpr = {read_excel(paste(inFile$datapath, ".xlsx", sep = ""), sheet = 1)})
   
+  data = eventReactive(eventExpr = input$go,
+               valueExpr = {read_excel(paste(input$file1$datapath, ".xlsx", sep = ""), sheet = 1)})
+  
+  output$estimation = renderPlot({
+    pred.clm = predict(lm(data()[,1] ~ data()[,2]), interval = "confidence", level = input$ci/100)
+    matplot(data()[,2], pred.clm, lty = c(1,2), type = "l", xlab = names(data())[2], ylab = names(data())[1])
+  })
+  
+  output$parameters_estimation = renderPrint({
+    summary(lm(data()[,1] ~ data()[,2]))
+  })
 }
 
 shinyApp(ui = ui, server = server)
